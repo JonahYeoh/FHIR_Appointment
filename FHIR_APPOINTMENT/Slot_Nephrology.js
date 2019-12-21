e010ac2dcc481d5e62af166ef9847d5b53711387
JonahYeoh <47838117+JonahYeoh@users.noreply.github.com>
2019-12-21 16:07:03 +0800

    Slot Limit

--- FHIR_APPOINTMENT/Slot_Nephrology.js
+++ FHIR_APPOINTMENT/Slot_Nephrology.js
@@ -5,6 +5,13 @@ var Slot = {
         "status": "generated",
         "div": "<div xmlns=\"http://www.w3.org/1999/xhtml\">\n\t\t\t25 Dec 2013 9:15am - 9:30am: \n\t\t</div>"
     },
+    "identifier": [
+        {
+          "use":"official",
+          "system": "http://example.org/identifiers/slotsLimit",
+          "value": "100"
+        }
+    ],
     "serviceCategory": [{
         "coding": [{
             "code": "27",
@@ -36,6 +43,7 @@ var Slot = {
     "status": "free",
     "start": "2013-12-25T09:15:00Z",
     "end": "2013-12-25T09:30:00Z",
+    "overbooked":false,
     "comment": "Assessments should be performed before requesting appointments in this slot."
 };
 
